94d82c4efa43c00b92bb80c3369eabf2ff43aa69
SonamSaraswat <sonamsaraswat777@gmail.com>
2024-09-13 11:35:51 +0530

    changes

--- pookie/src/components/Rightbar.jsx
+++ pookie/src/components/Rightbar.jsx
@@ -4,7 +4,7 @@ const Rightbar = () => {
   return (
     <div className='w-[25%] h-[90%] m-8 p-4 rounded bg-[#121212] overflow-y-scroll'>
         <div className='bg-[#242424]'>
-            <p className='text-white flex items-center justify-center font-semibold rounded bg-black'>Pookies Talk</p>
+            <p className='text-white flex items-center justify-center font-semibold rounded  bg-[#242424]'>Let's Talk</p>
        </div>
     </div>
        
--- pookie/src/components/Sidebar.jsx
+++ pookie/src/components/Sidebar.jsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
           </div>
         </div>
         <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
-          <h1>Create your first pookie playlist</h1>
+          <h1>Create your first playlist</h1>
           <p className='font-light'>It's easy we will help you</p>
           <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
         </div>
