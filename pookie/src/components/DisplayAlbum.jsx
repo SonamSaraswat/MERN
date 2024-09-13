c6c5c41e8d4368665d6cb4be58373a88b74bf507
SonamSaraswat <sonamsaraswat777@gmail.com>
2024-09-13 11:53:48 +0530

    changes

--- pookie/src/components/DisplayAlbum.jsx
+++ pookie/src/components/DisplayAlbum.jsx
@@ -28,8 +28,29 @@ const DisplayAlbum = () => {
     </div>
     <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
       <p><b className='mr-4'>#</b>Title</p>
+      <p>Album</p>
+      <p className='hidden sm:block'>Data Added</p>
+      <img className='m-auto w-4' src={assets.clock_icon} alt=""/>
+      </div>
+      <hr/>{
+        songsData.map((item,index)=>{
+         <div key={index} className='grid grid-cols-3 sm:gid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover-[#ffffff2b] cursor-pointer'>
+            <p className='text-white'>
+              <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
+              <img className='inline w-10 mr-5' src={item.image} alt="" />
+              {item.name}
+            </p>
 
-    </div>
+            <p className='text-[15px]'>{albumData.name}</p>
+            <p className='text-[15px] hidden sm:block'> 5 days ago</p>
+            <p className='text-[15px] text-center'>{item.duration}</p>
+
+          </div>
+
+        })
+      }
+
+   
     </>
   )
 }
